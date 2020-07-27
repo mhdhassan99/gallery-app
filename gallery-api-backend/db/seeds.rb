9980770e9567c24f780137ae3b07f76d1b851604
afrolambo <stephennoahgalvan@.com>
2020-07-27 15:50:15 -0400

    added base template for seeds

--- gallery-api-backend/db/seeds.rb
+++ gallery-api-backend/db/seeds.rb
@@ -10,20 +10,130 @@
 
 pictures = [
     {
-        title: "mountain", 
+        title: 'mountain', 
         like: 0,
         imageUrl: "https://cosmosmagazine.com/wp-content/uploads/2020/02/190218-mount-full-1440x810.jpg"
     },
 
     {
-        title: "oceans", 
+        title: 'oceans', 
         like: 0,
         imageUrl: "https://scx2.b-cdn.net/gfx/news/2019/bluefinancea.jpg"
     },
 
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    },
+
+    {
+        title: '', 
+        like: 0,
+        imageUrl: "",
+    }
  ]
 
-
-  pictures.each do |picture|
-    Picture.create!(picture)
+ 5.times do
+    User.create(name: Faker::User.name, location: Faker::User.location, age: Faker::User.age)
   end
+
+pictures.each do |picture|
+    Picture.create!(picture)
+end
